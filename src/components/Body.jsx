334907eff4b82fe401ea6f33d821acd9c2dc3238
Shikhar <vermashikhar20@gmail.com>
2024-03-28 11:04:00 +0530

    modified:   netlify.toml 	modified:   src/components/Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -22,7 +22,7 @@ const Body = () => {
 
     const fetchData = async() => {
         const url = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.193391&lng=79.082636&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        const data = await fetch(`/thing-proxy/${url}`);
+        const data = await fetch(url);
         const json = await data.json();
         
         setlistOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
