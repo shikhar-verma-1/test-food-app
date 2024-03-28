bfc3b6044fff606782dbec3f395cb8b82f5c1d6e
Shikhar <vermashikhar20@gmail.com>
2024-03-28 08:21:01 +0530

    modified:   src/components/Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -21,7 +21,7 @@ const Body = () => {
     },[]);
 
     const fetchData = async() => {
-        const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.193391&lng=79.082636&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.193391&lng=79.082636&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         
         setlistOfRestaurants(json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
